4c283d9b8d52e4f5c4452fbd784a1154435942da
aleksei@mazur <mazuralexey@gmail.com>
2022-04-22 02:51:20 +0300

    my posts view and link

--- posts/urls.py
+++ posts/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
-from posts.views import ItemsListView, ItemsPrivateListView, PostReaderView
+from posts.views import PostsListView, PrivatePostsListView, PostReaderView, UserPostView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', ItemsListView.as_view(), name='postslist'),
-    path('filtered/', ItemsPrivateListView.as_view(), name='secretpostslist'),
-    path('post/<post_id>', PostReaderView.as_view(), name='post')
+    path('', PostsListView.as_view(), name='postslist'),
+    path('filtered/', login_required(PrivatePostsListView.as_view()), name='secretpostslist'),
+    path('post/<post_id>', PostReaderView.as_view(), name='post'),
+    path('all/', UserPostView.as_view(), name='usersposts'),
 
 ]
 if settings.DEBUG:
--- posts/views.py
+++ posts/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from posts.models import PostItem, Category
+from authapp.models import User
 from posts.forms import PostForm
+from urllib import request
 
-
-class ItemsListView(ListView):
+class PostsListView(ListView):
     model = PostItem
     queryset = PostItem.objects.filter(public=True).order_by('-created_at')
     template_name = "postlist.html"
     title = 'Posts'
 
     def get_queryset(self):
-        queryset = super(ItemsListView, self).get_queryset()
+        queryset = super(PostsListView, self).get_queryset()
         category_id = self.kwargs.get('category_id')
         return queryset.filter(category_id=category_id) if category_id else queryset
 
     def get_context_data(self, **kwargs):
-        context = super(ItemsListView, self).get_context_data(**kwargs)
+        context = super(PostsListView, self).get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
+        context['posts'] = PostItem.objects.filter(public=True).order_by('-created_at')
         return context
 
 
-class ItemsPrivateListView(ItemsListView):
+class PrivatePostsListView(PostsListView):
     queryset = PostItem.objects.filter(private=True).order_by('-created_at')
     template_name = "secret_postlist.html"
     title = 'Secret Posts'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostsListView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['posts'] = PostItem.objects.filter(private=True).order_by('-created_at')
+        return context
 
 
 class PostReaderView(ListView):
@@ -38,5 +46,19 @@ class PostReaderView(ListView):
             'title': post.title,
             'categories': categories,
             'post': post
+        }
+        return render(request, self.template_name, context)
+
+
+class UserPostView(ListView):
+    form_class = PostForm
+    template_name = 'postlist.html'
+
+    def get(self, request):
+        user = User.objects.get(id=request.user.id)
+        posts = PostItem.objects.filter(author_id=user)
+        context = {
+            'user': user,
+            'posts': posts
         }
         return render(request, self.template_name, context)
